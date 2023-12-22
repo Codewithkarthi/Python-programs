2d9a1012fd0917fda5faa17484257d76ebf9ba04
Karthika Raj <148317926+Codewithkarthi@users.noreply.github.com>
2023-12-22 16:59:58 +0530

    Create Task2.py
    Combining the letters without repitations using python.

---
+++ Task2.py
@@ -0,0 +1,27 @@
+#method(1)
+#combinations of the letters "a," "b," and "c" without repetition.
+#you can use the combinations function from the itertools module.
+
+
+from itertools import combinations
+
+letters = ['a', 'b', 'c']
+
+combinations_list = []
+for r in range(1, len(letters) + 1):
+    combinations_list.extend(combinations(letters, r))
+
+combinations_result = [''.join(combination) for combination in combinations_list]
+
+print(combinations_result)
+
+#method(2)
+#simple method of combinating in python.
+
+from itertools import permutations
+
+letters = ['a', 'b', 'c']
+
+combinations = [''.join(p) for p in permutations(letters)]
+
+print(combinations)
