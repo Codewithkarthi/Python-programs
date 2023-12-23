ada89e07889493c17ae4e283e44709ba768e41a5
Karthika Raj <148317926+Codewithkarthi@users.noreply.github.com>
2023-12-23 12:00:14 +0530

    Create Task3.py
    Print the student name and the total marks

---
+++ Task3.py
@@ -0,0 +1,23 @@
+#print student name and their five subject total marks from the max 100 marks
+Name=int(input("enter your name"))
+
+Tamil=int(input("enter your tamil mark out off 100 :"))
+
+English=int(input("enter your english mark out off 100 :"))
+
+Maths=int(input("enter your maths mark out off 100 :"))
+
+Science=int(input("enter your science mark out off 100 :"))
+
+Social=int(input("enter your social mark out off 100 :"))
+
+Total-Tamil+English+Maths+Science+Soci al
+
+print("Student Name:",Name)
+  
+Print("Total Marks:", Total)
+
+Percentage-Total/500 *100
+
+Print("Total percentage:", percentage)
+
