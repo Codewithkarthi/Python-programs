874f9cb610e37f2e506a233e0202cef359fa0bc3
Karthika Raj <148317926+Codewithkarthi@users.noreply.github.com>
2023-12-25 12:41:22 +0530

    Create Task5.py
    Print the friends name with a given sentence.

---
+++ Task5.py
@@ -0,0 +1,8 @@
+#give the variables for three friends and print we all are together and spread love.
+# Three friends
+a = "Alice"
+b = "Bob"
+c = "Charlie"
+
+# Print a message
+print(f"{a}, {b}, and {c} - we all are together and spread love!")
