dc5861067278ff4775ba9a23e6a6383a2076e35f
Karthika Raj <148317926+Codewithkarthi@users.noreply.github.com>
2023-12-28 22:01:27 +0530

    Task8.py

---
+++ Task8.py
@@ -0,0 +1,9 @@
+#removing everything in the list and keeping only the desired goal.
+
+a=[]; 
+a=input("Enter list of your dreams job"));
+b=chr(input("Enter your favorite job"));
+if(a[i]==b) 
+print("your current favorite job is: ", b); 
+else print("not match your entry jobs ");
+
