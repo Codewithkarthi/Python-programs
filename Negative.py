63913031b8b17462c109dd3d78c26def8381661c
Karthika Raj <148317926+Codewithkarthi@users.noreply.github.com>
2024-01-03 19:20:07 +0530

    Pos-neg.py
    Checking whether the number is positive or negative.

---
+++ Negative.py
@@ -0,0 +1,9 @@
+#print whether the given number is positive or negative
+num=float(input("enter the number")
+if(num>0):
+  print("positive")
+elif(num==0):
+  print("zero")
+else:
+  print("negative")
+
