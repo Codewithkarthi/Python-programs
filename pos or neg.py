28b177b2cbd24dc21aa3be516570bb08b685af1b
Karthika Raj <148317926+Codewithkarthi@users.noreply.github.com>
2024-01-25 11:43:24 +0530

    pos or neg.py

---
+++ pos or neg.py
@@ -0,0 +1,9 @@
+num = float(input("Enter a number: "))
+# Input: 1.2
+if num > 0:
+   print("Positive number")
+elif num == 0:
+   print("Zero")
+else:
+   print("Negative number")
+#output: Positive number
