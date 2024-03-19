16ab70c9aa8042ce47c66b7692576cba398d835b
Karthika Raj <148317926+Codewithkarthi@users.noreply.github.com>
2024-03-19 13:13:30 +0530

    Create Vowel or constant.py

---
+++ Logical problems/Vowel or constant.py
@@ -0,0 +1,9 @@
+("Python program to check given character is a vowel or consonant")
+
+Ch=input(“Enter a character to check vowel or consonant :”)
+if(ch == ‘a’ or ch == ‘e’ or ch == ‘i’ or ch == ‘o’ or ch == ‘u’ or ch == ‘A’ or ch == ‘E’ or ch == ‘I’ or ch == ‘O’ or ch == ‘U’):
+#if ‘if’ condition satisfy
+print(“Given character”, ch ,”is vowel”)
+else:
+#if ‘if’ condition does not satisfy the condition
+print(“Given character”,ch, “is  consonant”)
