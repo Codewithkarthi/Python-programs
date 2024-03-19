dd30e8479865f1d0072f3d38f9ce0b045d2245f2
Karthika Raj <148317926+Codewithkarthi@users.noreply.github.com>
2024-03-19 13:27:15 +0530

    Create Task 11.py

---
+++ Task Questions/Task 11.py
@@ -0,0 +1,11 @@
+ #Generate a secret number under 10 and take a guess. Win with the right number or try again for the ultimate gaming fun.
+
+import random
+
+secret_number = random.randint(1, 10)
+guess = int(input("Guess the secret number (between 1 and 10): "))
+
+if guess == secret_number:
+    print("Congratulations! You guessed the secret number!")
+else:
+    print("Sorry, that's not the right number. The secret number was:", secret_number)
